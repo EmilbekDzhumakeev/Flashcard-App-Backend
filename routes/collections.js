c05fb25639fa746aaa1678ddf1503f2b4ea6ac0a
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-07-01 18:22:14 -0400

    modified routes/collections.js added GET All and GET By Id functionality Postman test successful

--- routes/collections.js
+++ routes/collections.js
@@ -21,5 +21,24 @@ router.post('/', async (req, res) => {
     } catch (ex) {
     return res.status(500).send(`Internal Server Error: ${ex}`); }
     });
+/////////////////////////////////////////////////////// GET all //////////////////////////// 
+router.get('/', async (req, res) => {
+  try {
+    const collections = await Collection.find();
+    return res.send(collections);
+  } catch (ex) {
+return res.status(500).send(`Internal Server Error: ${ex}`); }
+});
+////////////////////////////////////////////////////////////// GET By Id //////////////////////// 
+router.get('/:id', async (req, res) => {
+  try {
+      const collection = await Collection.findById(req.params.id);
+          if (!collection)
+              return res.status(400).send(`The collection with id "${req.params.id}" does not exist.`);
+    return res.send(collection);
+} catch (ex) {
+return res.status(500).send(`Internal Server Error: ${ex}`);
+}
+}); 
 ///////////////////////////////////////////////////////////////////////////////////////////////// 
 module.exports = router;
