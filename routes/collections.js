83ce04ebba61101dbda0c71d0e5cc014796c5f6d
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-07-01 18:33:48 -0400

    modified collections.js added PUT and DELETE functions Postman test successsful

--- routes/collections.js
+++ routes/collections.js
@@ -40,5 +40,38 @@ router.get('/:id', async (req, res) => {
 return res.status(500).send(`Internal Server Error: ${ex}`);
 }
 }); 
+////////////////////////////////////////////////////////////// PUT  //////////////////////// 
+router.put('/:id', async (req, res) => {
+  try {
+const { error } = validate(req.body);
+if (error) return res.status(400).send(error);
+const collection = await Collection.findByIdAndUpdate(
+  req.params.id,
+  {
+    term: req.body.term,
+    definition: req.body.definition,
+    item: req.body.item,  
+      },
+      { new: true }
+    );
+if (!collection)
+return res.status(400).send(`The collection with id "${req.params.id}" d
+oes not exist.`);
+    await collection.save();
+    return res.send(collection);
+  } catch (ex) {
+return res.status(500).send(`Internal Server Error: ${ex}`); }
+});  
+////////////////////////////////////////////////////////////// DELETE //////////////////////// 
+router.delete('/:id', async (req, res) => {
+  try {
+    const collection = await Collection.findByIdAndRemove(req.params.id);
+      if (!collection)
+      return res.status(400).send(`The collection with id "${req.params.id}" does not exist.`);
+      return res.send(collection);
+} catch (ex) {
+return res.status(500).send(`Internal Server Error: ${ex}`);
+}
+});
 ///////////////////////////////////////////////////////////////////////////////////////////////// 
 module.exports = router;
